fd63ff3a1a1c00a6e12ea2775e1e5b0b8bbb0438
sergeichern <sergeiche01@gmail.com>
2025-09-03 20:43:47 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function AddNewTask({ inputValue, onInputChange, onSubmit }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={onInputChange}
+        placeholder="Add a new task"
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default function App() {
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (newTask.trim()) {
+
+      const newItem = { id: Date.now(), text: newTask };
+      setTasks(prev => [...prev, newItem]);
+
+      setNewTask('');
+    }
+  }
+
+  function handleDelete(id) {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To-Do-List</h1>
       </header>
+      <AddNewTask inputValue={newTask}
+        onInputChange={handleInputChange}
+        onSubmit={handleSubmit}
+      />
+      <div className="task-list">
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.text}
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+
